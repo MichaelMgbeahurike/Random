f6531a6caf11ca5eb8c5c11e2e7c52916aa74047
Michael Mgbeahurike <mgbeahurikem@gmail.com>
2024-03-12 20:04:26 +0000

    Some change

--- Program1.java
+++ Program1.java
@@ -2,6 +2,8 @@ class Program1{
     public static void main(String [] a) {
         System.out.println("Hello People Hello People Hello People");
         System.out.println("People are moving mad");
+        System.out.println("Hello");
+
     }
 }
 	
