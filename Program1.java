16d8cf459cdb0f48ef4476537b2173c27b96a632
Michael Mgbeahurike <mgbeahurikem@gmail.com>
2024-03-12 19:05:20 +0000

    Adding

--- Program1.java
+++ Program1.java
@@ -1,6 +1,7 @@
 class Program1{
     public static void main(String [] a) {
         System.out.println("Hello People Hello People Hello People");
+        System.out.println("People are moving mad");
     }
 }
 	
